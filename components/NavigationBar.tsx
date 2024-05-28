ee1272b5208f5ae14e46e0b6f4e8258acf014c00
Damien Ostler <damienostler1@outlook.com>
2024-05-28 00:08:42 -0400

    Update NavigationBar.tsx

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -23,7 +23,6 @@ export default async function AuthButton() {
  
   // ...
 
-  const heads = headers()
   console.log(heads)
   const url = heads.get('next-url') 
   if(url == null) throw new Error('No URL found')
