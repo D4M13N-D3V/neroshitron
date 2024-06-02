02872252b57d95a4b3ead81ea9ca99f613ea8fa9
Damien Ostler <damienostler1@outlook.com>
2024-06-02 17:35:10 -0400

    feat: tags admin ui

--- app/api/galleries/tags/route.ts
+++ app/api/galleries/tags/route.ts
@@ -5,8 +5,29 @@ import { createClient } from "@/utils/supabase/server";
 export async function GET(request: Request) {
   const supabase = createClient();
   let { data: tags, error } = await supabase
-  .from('tags')
-  .select('*')
+    .from('tags')
+    .select('*')
+    .order('name', { ascending: true });
   return NextResponse.json(tags)
 }
-  
+
+export async function POST(request: Request) {
+  const supabase = createClient();
+  const data = await request.json();
+  const { data: tag, error } = await supabase.from('tags').insert({ name: data.tag }).single();
+  console.log(error)
+  if (error) {
+    return NextResponse.error();
+  }
+  return NextResponse.json(tag);
+}
+
+export async function PUT(request: Request) {
+  const supabase = createClient();
+  const data = await request.json();
+  const { data: tag, error } = await supabase.from('tags').delete().eq('name', data.tag).single();
+  if (error) {
+    return NextResponse.error();
+  }
+  return NextResponse.json(tag);
+}
