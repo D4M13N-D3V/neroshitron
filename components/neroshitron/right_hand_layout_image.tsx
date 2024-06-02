78a25ab398d97cac271be70dd409d2bbf8bc3459
Damien Ostler <damienostler1@outlook.com>
2024-06-02 07:51:11 -0400

    Update right_hand_layout_image.tsx

--- components/neroshitron/right_hand_layout_image.tsx
+++ components/neroshitron/right_hand_layout_image.tsx
@@ -5,7 +5,7 @@ const RightHandLayoutImage: React.FC = () => {
     return (
         <div className="fixed w-full h-full overflow-hidden z-0 animate-fade-left animate-once animate-duration-[2000ms] animate-normal animate-fill-forwards">
             <img
-                src="gallery_girl.png"
+                src="./gallery_girl.png"
                 className="float-right object-cover h-screen w-full lg:w-5/6 xl:w-3/6 opacity-50 overflow-hidden"
                 alt="Background"
             />
