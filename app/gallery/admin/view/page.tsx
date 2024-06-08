d94f3b825be795900cd116f5b0dd0f3925f1cf50
Damien Ostler <damienostler1@outlook.com>
2024-06-08 18:53:10 -0400

    fix: gallery admin page

--- app/gallery/admin/view/page.tsx
+++ app/gallery/admin/view/page.tsx
@@ -139,43 +139,45 @@ function PageComponent() {
                         ></GalleryThumbnail>
                     )}
                 </div>
-                <div className="w-full flex opacity-90 backdrop-blur-lg bg-primary  shadow-lg p-8 pb-0 rounded">
-                    <input
-                        type="text"
-                        className="mb-8 mr-2 rounded-md bg-secondary p-2 w-1/2 text-white"
-                        placeholder="Gallery Name"
-                        value={galleryName}
-                        onChange={(e) => setGalleryName(e.target.value)}
-                    /><div className="w-1/6">
-                    <button
-                        onClick={() => deleteGallery()}
-                        className="text-center w-full bg-error hover:bg-error-light text-white rounded-md p-2 flex items-center justify-center"
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} 
-                        stroke="currentColor" className="md:hidden size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                        </svg>
-                  
-                        <span className="md:block hidden">Delete</span>
-                    </button>
-                </div>
-                <div className="w-1/6">
-                    <button
-                        onClick={() => (router.push("/gallery/admin"))}
-                        className="w-full bg-error-dark hover:bg-error text-white rounded-md p-2 ml-2 flex items-center justify-center"
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} 
-                        stroke="currentColor" className="md:hidden size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
-                        </svg>
-                
-                        <span className="md:block hidden">Back</span>
-                    </button>
-                </div>
-                    <div className="w-1/4">
-                        <button onClick={()=>{updateGallery()}} className="w-full bg-warning hover:bg-warning-light text-white rounded-md p-2 ml-4">
-                            <span>Update</span>
-                        </button>
+                <div className="w-full opacity-90 backdrop-blur-lg bg-primary  shadow-lg p-8 pb-0 rounded">
+                    <span className="text-2xl">Editing Gallery</span>
+                    <div className="w-full flex justify-end">
+                        <div className="w-1/2 flex">
+                            <input
+                                type="text"
+                                className="mb-8 mr-2 rounded-md bg-secondary p-2 w-full text-white"
+                                placeholder="Gallery Name"
+                                value={galleryName}
+                                onChange={(e) => setGalleryName(e.target.value)}
+                            />
+                        </div>
+                        <div className="w-1/2 flex">
+                            <button
+                                onClick={() => deleteGallery()}
+                                className="h-10 text-center w-full bg-error hover:bg-error-light text-white rounded-md p-2 flex items-center justify-center"
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} 
+                                stroke="currentColor" className="md:hidden size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                </svg>
+                        
+                                <span className="md:block hidden">Delete</span>
+                            </button>
+                            <button
+                                onClick={() => (router.push("/gallery/admin"))}
+                                className="h-10 w-full bg-error-dark hover:bg-error text-white rounded-md p-2 ml-2 flex items-center justify-center"
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} 
+                                stroke="currentColor" className="md:hidden size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
+                                </svg>
+                        
+                                <span className="md:block hidden">Back</span>
+                            </button>
+                            <button onClick={()=>{updateGallery()}} className="h-10 w-full bg-warning hover:bg-warning-light text-white rounded-md p-2 ml-2">
+                                <span>Update</span>
+                            </button>
+                        </div>
                     </div>
                 </div>
                 <div className="w-full lg:flex opacity-90 backdrop-blur-lg bg-primary  shadow-lg p-8 pt-0 rounded">
